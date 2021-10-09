2df7b6e444b6b4becde8c66065c615574b87879d
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2021-10-08 20:42:23 -0400

    icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './Components/Header.js'
+import Icons from './Components/Icons';
 
 function App() {
   return (
+    <>
     <Header />
+    </>
   )
 }
 
---
+++ src/Components/Icons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const Icons = () => {
+    return (
+        <Box>
+        <FacebookIcon />
+        <InstagramIcon />
+        <TwitterIcon />
+        </Box>
+    )
+}
+
+export default Icons
