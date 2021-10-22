3bc9c2b9cf1fe21787648f445c55d4dbf81a38b3
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2021-10-22 10:20:33 -0400

    ThemeProvider in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import Header from './Components/Header.js'
-import Icons from './Components/Icons';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#fef1e6',
+      main: '#f9d5a7',
+      dark: '#ffb085',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#b85672',
+      main: '#852747',
+      dark: '#540020',
+      contrastText: '#000',
+    },
+  },
+});
+
+
 
 function App() {
   return (
-    <>
+  
+    <ThemeProvider theme={theme}>
     <Header />
-    </>
+    </ThemeProvider>
+  
   )
 }
 
