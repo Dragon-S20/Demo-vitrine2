149e86928197f4d95a736a199bf5522564cdab27
Dragon-S20 <promoetherjs.dragons20@gmail.com>
2021-10-25 12:42:01 -0400

    error 404

---
+++ src/Components/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Info404 = () => {
+    return(
+    <section>
+      <>
+  <p>Oups !</p>
+  <h1>La page que vous recherchez semble introuvable.</h1>
+  <p>Code erreur : <strong>404</strong></p>
+      </>
+</section>)
+}
+
+export default Info404
